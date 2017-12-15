ef8b251b2a866b202bcff6a0c10e15fdacfecc55
TetianaKryvets <tetiana.kryvets@itera.no>
2017-12-15 11:42:19 +0200

    Setenity test - updated

--- src/test/java/GoogleTests/SerenityExample/GoogleSerenityPages.java
+++ src/test/java/GoogleTests/SerenityExample/GoogleSerenityPages.java
@@ -3,12 +3,6 @@ package GoogleTests.SerenityExample;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
-import net.thucydides.core.annotations.Managed;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
 /**
@@ -38,37 +32,4 @@ public class GoogleSerenityPages extends PageObject {
 
 
 
-
-
-//    public String clickOnTheFirstPicture() {
-//        WebElement firstPictureButton = BaseTests.driver.findElement(By.xpath("(//div[@class='rg_bx rg_di rg_el " +
-//                "ivg-i']/a[@class='rg_l'])[1]"));
-//        firstPictureButton.click();
-//        return firstPictureButton.getAttribute("href");
-//    }
-//
-//    public String openInNewWindow() throws InterruptedException {
-//        WebElement firstPictureButton = BaseTests.driver.findElement(By.xpath("(//div[@class='rg_bx rg_di rg_el " +
-//                "ivg-i']/a[@class='rg_l'])[1]"));
-//
-//        Actions newAction = new Actions(BaseTests.driver);
-//        newAction.keyDown(Keys.SHIFT).click(firstPictureButton).keyUp(Keys.SHIFT).build().perform();
-//
-//        String classValue = firstPictureButton.getAttribute("href");
-//        return classValue;
-//    }
-//
-//    public void switchToNewWindow() {
-//        // Store the current window handle
-//        String winHandleBefore = BaseTests.driver.getWindowHandle();
-//        // Perform the click operation that opens new window
-//
-//        // Switch to new window opened
-//        for(String winHandle : BaseTests.driver.getWindowHandles()){
-//            BaseTests.driver.switchTo().window(winHandle);
-//        }
-
-        // Perform the actions on new window
-
-    
 }
